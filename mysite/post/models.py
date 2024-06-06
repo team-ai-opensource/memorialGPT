b3ba01fa2211a446d04a46432cdcb06235e6b01d
hyeonho cho <111675679+aesthetics-of-record@users.noreply.github.com>
2024-06-06 16:21:27 +0900

    feat: 데이터베이스 추가

--- mysite/post/models.py
+++ mysite/post/models.py
@@ -5,3 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=75)
     content = models.TextField()
     data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
