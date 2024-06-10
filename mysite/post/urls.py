00d1127b2e6fea0e3d59013dbc1e8c1f85dd6613
hyeonho cho <111675679+aesthetics-of-record@users.noreply.github.com>
2024-06-10 10:42:15 +0900

    feat: 채팅방 ui 수정 및 삭제기능 추가

--- mysite/post/urls.py
+++ mysite/post/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create', views.create_post),
     path('update/<int:id>', views.update_post),
     path('chat', views.post_chat),
+    path('delete/<int:id>', views.delete_post),
 ]
